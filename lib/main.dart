9f427f6385f17d8e95d2d70e0638a27f8d2929f3
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-06-15 16:19:47 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vamos Cozinhar?',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar?'),
+      ),
+      body: Center(
+        child: Text('Navegar é preciso!!'),
+      ),
+    );
+  }
+}
