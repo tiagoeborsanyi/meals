3d30bde6dd4a08416ca6df0f32274092a66b0b7a
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-06-16 17:06:06 -0300

    add style on mealItem component

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -7,10 +7,39 @@ class MealItem extends StatelessWidget {
 
   const MealItem(this.meal);
 
+  void _selectMeal() {}
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Text(meal.title),
+    return InkWell(
+      onTap: _selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
