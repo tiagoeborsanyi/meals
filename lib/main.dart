f7dbe8a828f002f88ca4461533f5820ab58bbca3
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-06-16 18:03:08 -0300

    created simple component mealDetailScreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
+import 'screens/meal_detail_screen.dart';
 import 'utils/app_routes.dart';
 
 void main() {
@@ -29,6 +30,7 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (ctx) => CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
       },
     );
   }
